3534ba706fafabeaaa5c8a68ac8c532d1eab3371
Nastya-Kananovich <a.s.kananovich@students.psu.by>
2025-07-03 19:32:57 +0300

    addcondition level6ex2.js

--- level6ex2.js
+++ level6ex2.js
@@ -1,3 +1,9 @@
+//Write a function that will take in any array and reverse it.
+//NOTES:
+//Array should be reversed in place! (no need to return it)
+//Usual builtins have been deactivated. Don't count on them.
+//You'll have to do it fast enough, so think about performances
+
 function reverseArray(arr) {
     let left = 0;
     let right = arr.length - 1;
