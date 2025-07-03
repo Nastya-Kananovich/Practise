3fad773dba1c5df5328fbc10fae29a8779bd0021
Nastya-Kananovich <a.s.kananovich@students.psu.by>
2025-07-03 20:00:02 +0300

    Update level6ex3.js

--- level6ex3.js
+++ level6ex3.js
@@ -1,9 +1,10 @@
-//In this Kata, you must create a function that takes an amount of US currency in cents, and returns a dictionary/hash which shows the least amount of coins used to make up that amount.
-//The only coin denominations considered in this exercise are: Pennies (1¢), Nickels (5¢), Dimes (10¢) and Quarters (25¢).
-//Therefore, the dictionary returned should contain exactly 4 key/value pairs.
-//Notes:
-//If the function is passed either 0 or a negative number, the function should return the dictionary with all values equal to 0.
-//If a float is passed into the function, its value should be rounded down, and the resulting dictionary should never contain fractions of a coin.
+/*In this Kata, you must create a function that takes an amount of US currency in cents, and returns a dictionary/hash which shows the least amount of coins used to make up that amount.
+The only coin denominations considered in this exercise are: Pennies (1¢), Nickels (5¢), Dimes (10¢) and Quarters (25¢).
+Therefore, the dictionary returned should contain exactly 4 key/value pairs.
+Notes:
+If the function is passed either 0 or a negative number, the function should return the dictionary with all values equal to 0.
+If a float is passed into the function, its value should be rounded down, and the resulting dictionary should never contain fractions of a coin.
+*/
 
 function looseChange(cents) {
     cents = Math.floor(cents);
